58dea9a55fb82965967dc15a2a87eae290269249
Javi Velasco <velascoalcaidejavi@gmail.com>
2025-10-04 21:51:50 +0200

    LoginForm style update

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,19 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "@/styles/Login/_LoginForm.scss";
 
+
 const LoginForm: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleEnter = () => {
+        // Por ahora solo navega a /home sin login real
+        navigate("/home");
+    };
+
     return (
-        <form className="login-form">
-            <h2>Inicia Sesión</h2>
-            <div className="form-group">
-                <label htmlFor="username">Usuario</label>
-                <input type="text" id="username" placeholder="Tu nombre de usuario" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Contraseña</label>
-                <input type="password" id="password" placeholder="Tu contraseña" />
-            </div>
-        </form>
+        <div className="login-form-div">
+            <form className="login-form">
+                <div className="form-group">
+                    <label htmlFor="username">Usuario</label>
+                    <input type="text" id="username" placeholder="Tu nombre de usuario" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Contraseña</label>
+                    <input type="password" id="password" placeholder="Tu contraseña" />
+                </div>
+            </form>
+            <button className="login-button" onClick={handleEnter}>
+                Entrar
+            </button>
+        </div>
     );
 };
 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,33 +1,21 @@
 import "@/styles/Pages/_LoginPage.scss";
-import { useNavigate } from "react-router-dom";
 import LoginBG from "../components/Login/LoginBG";
 import LoginForm from "../components/Login/LoginForm";
 import { Logo } from "../components/Logo";
 
 function LoginPage() {
-    const navigate = useNavigate();
 
-    const handleEnter = () => {
-        // Por ahora solo navega a /home sin login real
-        navigate("/home");
-    };
 
     return (
         <div className="login-page">
             <LoginBG />
-
             <div className="login-content">
                     <Logo />
                     <LoginForm />
-                    <button className="login-button" onClick={handleEnter}>
-                        Entrar
-                    </button>
             </div>
             <div className="neon-bg">
                 <div className="neon-border"></div>
             </div>
-            
-
         </div>
     );
 }
